93f4a07500732336c30b1749f1e23dcc276e6c79
lusianaferisca04 <lusianaferisca.04@gmail.com>
2025-11-26 14:48:53 +0700

    first

---
+++ forms/contact.php
@@ -0,0 +1,38 @@
+<div class="col-lg-7">
+  <form action="https://formsubmit.co/lusianaferiscaa@gmail.com" method="POST" class="php-email-form" data-aos="fade-up" data-aos-delay="200">
+
+    <!-- FormSubmit Settings -->
+    <input type="hidden" name="_captcha" value="false">
+    <input type="hidden" name="_template" value="table">
+    <input type="hidden" name="_next" value="https://yourwebsite.com/thankyou.html"> 
+    <!-- ganti dengan URL websitemu jika ingin redirect -->
+
+    <div class="row gy-4">
+
+      <div class="col-md-6">
+        <label for="name-field" class="pb-2">Your Name</label>
+        <input type="text" name="name" id="name-field" class="form-control" required>
+      </div>
+
+      <div class="col-md-6">
+        <label for="email-field" class="pb-2">Your Email</label>
+        <input type="email" class="form-control" name="email" id="email-field" required>
+      </div>
+
+      <div class="col-md-12">
+        <label for="subject-field" class="pb-2">Subject</label>
+        <input type="text" class="form-control" name="subject" id="subject-field" required>
+      </div>
+
+      <div class="col-md-12">
+        <label for="message-field" class="pb-2">Message</label>
+        <textarea class="form-control" name="message" rows="10" id="message-field" required></textarea>
+      </div>
+
+      <div class="col-md-12 text-center">
+        <button type="submit">Send Message</button>
+      </div>
+
+    </div>
+  </form>
+</div>
